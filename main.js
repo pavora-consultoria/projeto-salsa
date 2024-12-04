60aa670be5677bc25a5829843f740e0d56188dbc
raiedev <thaisdivino21@gmail.com>
2024-12-04 04:10:13 -0300

    Finalizando projeto, ajustes de layout, nova função de reserva, avaliação e responsividade

--- main.js
+++ main.js
@@ -1,3 +1,84 @@
+const nome = document.getElementById("nome");
+const telefone = document.getElementById("telefone");
+const quantidadePessoas = document.getElementById("quantidade");
+const data = document.getElementById("data");
+const horario = document.getElementById("horarios");
+const botaoConfirmar = document.getElementById("btn-sucesso");
+const criarReserva = document.getElementById("criarReserva");
+const modalSucesso = new bootstrap.Modal(
+  document.getElementById("modal-sucesso")
+);
+let currentScrollPosition = 0;
+const nomeAvaliacao = document.getElementById("nomeAvaliacao");
+const comentarioAvaliacao = document.getElementById("comentarioAvaliacao");
+const enviarAvaliacao = document.getElementById("enviar");
+
+function scrollNext() {
+  const feedbackContainer = document.getElementById("feedback");
+  const itemWidth = document.querySelector(".avaliacao1").offsetWidth + 15;
+  const maxScroll =
+    feedbackContainer.scrollWidth - feedbackContainer.offsetWidth;
+
+  if (currentScrollPosition < maxScroll) {
+    currentScrollPosition += itemWidth;
+    feedbackContainer.style.transform = `translateX(-${currentScrollPosition}px)`;
+  }
+}
+
+function scrollPrev() {
+  const feedbackContainer = document.getElementById("feedback");
+  const itemWidth = document.querySelector(".avaliacao1").offsetWidth + 15;
+
+  if (currentScrollPosition > 0) {
+    currentScrollPosition -= itemWidth;
+    feedbackContainer.style.transform = `translateX(-${currentScrollPosition}px)`;
+  }
+}
+
+function validacaoAvaliacao() {
+  if (
+    nomeAvaliacao.value.trim() === "" ||
+    comentarioAvaliacao.value.trim() === ""
+  ) {
+    enviarAvaliacao.disabled = true;
+    enviarAvaliacao.style.backgroundColor = "gray";
+    enviarAvaliacao.style.color = "#ffffffa4";
+    enviarAvaliacao.style.border = "1px solid gray";
+  } else {
+    enviarAvaliacao.disabled = false;
+    enviarAvaliacao.style.backgroundColor = "#ffde59";
+    enviarAvaliacao.style.color = "black";
+  }
+}
+
+function criarAvaliacao() {
+  const feedbacks = document.getElementById("feedback");
+
+  const novaAvaliacao = `
+    <div class="avaliacao1">
+      <p class="pessoa">${nomeAvaliacao.value}</p>
+      <img src="https://ik.imagekit.io/mnz3yzqzot/estrelas.png?updatedAt=1732428668009" alt="Avaliação cinco estrelas" class="estrela" />
+      <p>${comentarioAvaliacao.value}</p>
+    </div>`;
+
+  feedbacks.insertAdjacentHTML("beforeend", novaAvaliacao);
+
+  const feedbackContainer = document.getElementById("feedback");
+  const itemWidth = document.querySelector(".avaliacao1").offsetWidth + 15;
+  const maxScroll =
+    feedbackContainer.scrollWidth - feedbackContainer.offsetWidth;
+
+  if (currentScrollPosition < maxScroll) {
+    currentScrollPosition += itemWidth;
+    feedbackContainer.style.transform = `translateX(-${currentScrollPosition}px)`;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const feedbackContainer = document.getElementById("feedback");
+  feedbackContainer.style.transform = "translateX(0px)";
+});
+
 function formatarData(data) {
   const dia = String(data.getDate()).padStart(2, "0");
   const mes = String(data.getMonth() + 1).padStart(2, "0");
@@ -5,79 +86,70 @@ function formatarData(data) {
   return `${dia}/${mes}/${ano}`;
 }
 
-async function enviarSMS() {
-  const nome = document.getElementById("nome").value;
-  const celular = document.getElementById("telefone").value;
-  const quantidadePessoas = document.getElementById("quantidade").value;
-  const data = document.getElementById("data").value;
-  const dataSelecionada = new Date(data);
-  const dataFormatada = formatarData(dataSelecionada);
-  const horario = document.getElementById("horarios").value;
+function gerarReserva() {
+  let reserva = "";
+  const caracteres = "0123456789";
 
-  const mensagem = `Olá ${nome}! \nSua reserva no Salsa foi confirmada para a data ${dataFormatada} ás ${horario}hrs para ${quantidadePessoas} pessoas. \n\nEndereço: Av. Nossa Senhora de Loreto, 1050,Vila Medeiros - Zona Norte - SP. \n\nCaso precise alterar sua reserva, entre em contato conosco. \nTe aguardamos!`;
-
-  try {
-    const response = await fetch("http://localhost:3000/send-sms", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        to: `+55${celular}`,
-        body: mensagem,
-      }),
-    });
-
-    const data = await response.json();
-
-    if (data.sid) {
-      document.body.innerHTML += `
-    <div id="modal-backdrop" style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5); z-index: 1040;"></div>
-    <div class="modal show" id="modal-sucesso" data-bs-backdrop="static" aria-modal="true" role="dialog" style="display: block; z-index: 1050;">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-body">
-                    <p>Reserva confirmada com sucesso! <br>
-Agradecemos pela escolha e esperamos vê-lo em breve!</p>
-                </div>
-            </div>
-        </div>
-    </div>`;
-
-      setTimeout(() => {
-        const modal = document.getElementById("modal-sucesso");
-        if (modal) {
-          modal.style.display = "none";
-          document.getElementById("modal-backdrop").remove();
-        }
-      }, 3000);
-    } else {
-      document.body.innerHTML += `
-    <div id="modal-backdrop" style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5); z-index: 1040;"></div>
-    <div class="modal show" id="modal-sucesso" data-bs-backdrop="static" aria-modal="true" role="dialog" style="display: block; z-index: 1050;">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-body">
-                    <p>Erro ao realizar a reserva! <br>
-Por favor, verifique o número de telefone e tente novamente.</p>
-                </div>
-            </div>
-        </div>
-    </div>`;
-
-      console.log(data.error);
-
-      setTimeout(() => {
-        const modal = document.getElementById("modal-sucesso");
-        if (modal) {
-          modal.style.display = "none";
-          document.getElementById("modal-backdrop").remove();
-        }
-      }, 3000);
-    }
-  } catch (error) {
-    console.error("Erro de conexão:", error);
+  for (let i = 0; i < 4; i++) {
+    const indiceAleatorio = Math.floor(Math.random() * caracteres.length);
+    reserva += caracteres[indiceAleatorio];
   }
+
+  return reserva;
+}
+
+function quantidadePessoasTexto(pessoas) {
+  if (pessoas > 1) {
+    return "pessoas";
+  } else {
+    return "pessoa";
+  }
+}
+
+function validacao() {
+  if (
+    nome.value.trim() === "" ||
+    telefone.value.trim() === "" ||
+    data.value.trim() === "" ||
+    horario.value.trim() === ""
+  ) {
+    botaoConfirmar.disabled = true;
+    botaoConfirmar.style.backgroundColor = "gray";
+    botaoConfirmar.style.color = "#ffffffa4";
+    botaoConfirmar.style.border = "1px solid gray";
+  } else {
+    botaoConfirmar.disabled = false;
+    botaoConfirmar.style.backgroundColor = "green";
+    botaoConfirmar.style.color = "white";
+  }
+}
+
+function confirmaReserva() {
+  const dataSelecionada = new Date(data.value);
+  const dataFormatada = formatarData(dataSelecionada);
+  const numReserva = gerarReserva();
+  const quantPessoasTexto = quantidadePessoasTexto(quantidadePessoas);
+  const textoOla = document.getElementById("ola");
+  const textoMensagem = document.getElementById("mensagem");
+
+  const mensagem = `Sua reserva de número <b>${numReserva}</b> foi confirmada no Restaurante Salsa para a data ${dataFormatada} ás ${horario.value}hrs para ${quantidadePessoas.value} ${quantPessoasTexto}.<br><br>Caso precise alterar sua reserva, entre em contato conosco.`;
+
+  textoOla.innerText = `Olá ${nome.value}!`;
+  textoMensagem.innerHTML = `${mensagem}`;
+
+  modalSucesso.show();
+
+  setTimeout(() => {
+    modalSucesso.hide();
+  }, 6000);
+
+  nome.value = "";
+  telefone.value = "";
+  quantidadePessoas.value = "";
+  horario.value = "";
+  data.value = "";
+
+  validacao();
 }
 
 const produtos = [
@@ -148,39 +220,52 @@ function fecharMenuHamburguer() {
 }
 
 menuHamburguer();
-document.addEventListener("DOMContentLoaded", gerarCards);
 
+validacao();
+validacaoAvaliacao();
+nome.addEventListener("input", validacao);
+telefone.addEventListener("input", validacao);
+horario.addEventListener("change", validacao);
+data.addEventListener("input", validacao);
+nomeAvaliacao.addEventListener("input", validacaoAvaliacao);
+comentarioAvaliacao.addEventListener("input", validacaoAvaliacao);
 
 // sam
 
 let selectedRating = 0;
 
-document.querySelectorAll('.star').forEach(star => {
-  star.addEventListener('click', function () {
+document.querySelectorAll(".star").forEach((star) => {
+  star.addEventListener("click", function () {
     // Remove a classe "active" de todas as estrelas
-    document.querySelectorAll('.star').forEach(s => s.classList.remove('active'));
+    document
+      .querySelectorAll(".star")
+      .forEach((s) => s.classList.remove("active"));
 
     // Adiciona a classe "active" nas estrelas até a selecionada
-    this.classList.add('active');
+    this.classList.add("active");
     selectedRating = this.dataset.value;
 
     for (let i = 1; i < this.dataset.value; i++) {
-      document.querySelector(`.star[data-value="${i}"]`).classList.add('active');
+      document
+        .querySelector(`.star[data-value="${i}"]`)
+        .classList.add("active");
     }
   });
 });
 
-document.getElementById('commentForm').addEventListener('submit', function (e) {
+document.getElementById("fale").addEventListener("submit", function (e) {
   e.preventDefault(); // Impede o envio do formulário
 
-  const commentInput = document.getElementById('commentInput');
-  const commentList = document.getElementById('commentList');
+  const commentInput = document.getElementById("commentInput");
+  const commentList = document.getElementById("commentList");
 
-  if (commentInput.value.trim() !== '' && selectedRating > 0) {
+  if (commentInput.value.trim() !== "" && selectedRating > 0) {
     // Cria um novo item de lista para o comentário
-    const newComment = document.createElement('li');
+    const newComment = document.createElement("li");
     newComment.innerHTML = `
-      <div class="rating-display">Avaliação: ${'★'.repeat(selectedRating)}${'☆'.repeat(5 - selectedRating)}</div>
+      <div class="rating-display">Avaliação: ${"★".repeat(
+        selectedRating
+      )}${"☆".repeat(5 - selectedRating)}</div>
       <p>${commentInput.value}</p>
     `;
 
@@ -188,11 +273,14 @@ document.getElementById('commentForm').addEventListener('submit', function (e) {
     commentList.appendChild(newComment);
 
     // Limpa o campo de entrada e o rating
-    commentInput.value = '';
-    document.querySelectorAll('.star').forEach(s => s.classList.remove('active'));
+    commentInput.value = "";
+    document
+      .querySelectorAll(".star")
+      .forEach((s) => s.classList.remove("active"));
     selectedRating = 0;
   } else {
-    alert('Por favor, insira um comentário e uma avaliação!');
+    alert("Por favor, insira um comentário e uma avaliação!");
   }
 });
 
+document.addEventListener("DOMContentLoaded", gerarCards);
